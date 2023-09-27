514ab4435e3e7e18783e480a6d97d190fdac1696
David Janda <djanda97@gmail.com>
2023-09-27 16:22:00 -0700

    Move ValidParenthesesTest to test directory

---
+++ src/test/java/leetcode_solutions/ValidParenthesesTest.java
@@ -0,0 +1,35 @@
+package leetcode_solutions;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+// https://leetcode.com/problems/valid-parentheses/
+class ValidParenthesesTest {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(' -> stack.push(')');
+                case '{' -> stack.push('}');
+                case '[' -> stack.push(']');
+                default -> {
+                    if (stack.isEmpty() || stack.pop() != c) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    @Test
+    void testIsValid() {
+        assertTrue(isValid("(){}[]"));
+        assertFalse(isValid("{[)"));
+    }
+}
